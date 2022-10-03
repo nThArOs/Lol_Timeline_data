3e20da2862bbbebc1b5ed5111f87cf9155b3156f
nThArOs <60221139+nThArOs@users.noreply.github.com>
2022-10-03 14:02:35 +0200

    commit dataset

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 def get_match_history(summonerName, player_region):
     match_history = lol_watcher.match.matchlist_by_puuid(region = player_region,
-                                                         puuid = summoner['puuid'])
+                                                         puuid = summonerName['puuid'])
     return match_history
 
 def get_challenger_leadboar(queue_type, player_region):
@@ -88,9 +88,6 @@ def flatten(l):
 
 def match_info(my_region, matchId):
     match_info = lol_watcher.match.by_id(my_region, matchId)
-
-
-    match_info = lol_watcher.match.by_id(my_region, matchId)
     match_detail = match_info['info']
     lol_dataset = []
     lol_dataset_row = {}
@@ -102,16 +99,18 @@ def match_info(my_region, matchId):
     lol_dataset_row['gameDuration'] = match_detail['gameDuration']
     lol_dataset_row['blueWin'] = match_detail['teams'][0]['win']
     lol_dataset_row['redWin'] = match_detail['teams'][1]['win']
+
+    match_metadata = pd.DataFrame.from_dict([lol_dataset_row])
     player_list = pd.DataFrame(match_detail['participants'])
     player_list = player_list.sort_values(by=['individualPosition'],ascending=False)
-    #player_list["individualPosition","teamId","summonerName","teamPosition",]
 
+    #PRINT OPTION
     pd.set_option('display.max_columns', None)  # or 1000
     pd.set_option('display.max_rows', None)  # or 1000
     pd.set_option('display.max_colwidth', None)  # or 199
     #print(player_list)
-    #DROP LES COLUMNS RELOU
 
+    #DROP LES COLUMNS RELOU
     player_list.drop('challenges', inplace =True, axis=1)
     player_list.drop('perks', inplace=True, axis=1)
 
@@ -122,33 +121,34 @@ def match_info(my_region, matchId):
     midBlue= blueTeam[blueTeam['teamPosition']=="MIDDLE"]
     adcBlue= blueTeam[blueTeam['teamPosition']=="BOTTOM"]
     supportBlue= blueTeam[blueTeam['teamPosition']=="UTILITY"]
-
     redTeam = player_list[player_list['teamId'] == 200]
-    topRed = blueTeam[blueTeam['teamPosition'] == "TOP"]
-    junglerRed = blueTeam[blueTeam['teamPosition'] == "JUNGLE"]
-    midRed = blueTeam[blueTeam['teamPosition'] == "MIDDLE"]
-    adcRed = blueTeam[blueTeam['teamPosition'] == "BOTTOM"]
-    supportRed = blueTeam[blueTeam['teamPosition'] == "UTILITY"]
+    topRed = redTeam[redTeam['teamPosition'] == "TOP"]
+    junglerRed = redTeam[redTeam['teamPosition'] == "JUNGLE"]
+    midRed = redTeam[redTeam['teamPosition'] == "MIDDLE"]
+    adcRed = redTeam[redTeam['teamPosition'] == "BOTTOM"]
+    supportRed = redTeam[redTeam['teamPosition'] == "UTILITY"]
 
-    teamBlue = pd.concat({'topBlue':topBlue,'junglerBlue':junglerBlue,'midBlue':midBlue,'adcBlue':adcBlue,
-                          'supportBlue':supportBlue}, axis=1)
-    teamRed = pd.concat({'topRed': topRed, 'junglerRed': junglerRed, 'midRed': midRed, 'adcRed': adcRed,
-                          'supportRed': supportRed}, axis=1)
-    play_data = pd.concat({'teamBlue':teamBlue,"teamRed":teamRed}, axis=1)
 
-    caca = pd.MultiIndex.from_frame(player_list)
-    print(caca)
+    team = [topRed,junglerRed,midRed,adcRed,supportRed,topBlue,junglerBlue,midBlue,adcBlue,supportBlue]
+    teamConcat = pd.concat(team)
+
+    default_value = matchId
+    teamConcat.insert(2,'gameId',default_value)
+
+    team_multi = teamConcat.set_index(['gameId','teamId','teamPosition'])
+    team_multi.to_csv("math_detail_test.csv",index=False)
+    df = pd.read_csv("math_detail_test.csv")
+    print(team_multi)
+    print(df)
+
+
 
 
 
     return
 
-
-
-
-
 #-----------------------------MAIN-------------------------------------#
-lol_watcher = LolWatcher('RGAPI-d3eb2b41-ddf8-4d2b-a851-51c42803b027')
+lol_watcher = LolWatcher('RGAPI-34f7bc11-170d-4423-a20e-9f3f878c62b6')
 my_region = 'euw1'
 queue_type = "RANKED_SOLO_5x5"
 #print(get_challenger_leadboar(queue_type, my_region))
